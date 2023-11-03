b3a3648af268aa141260d1670d88bccba2dd8e1b
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-11-03 13:28:50 +0200

    feat: complete tasks for amenities

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,24 @@
+//Ensure script only runs when dom is loaded
+const $ = window.$;
+
+$(document).ready(function () {
+  const amenities = {};
+  let newList = [];
+  const check = $(".amenities", input[type = "checkbox"]);
+  check.prop("checked", false);
+  check.change(function () {
+    const dataID = $(this).attr("data-id");
+    const dataName = $(this).attr("data-name");
+    if (this.checked) {
+      amenities[dataID] = dataName;
+    } else {
+      delete (amenities[dataID]);
+    }
+    for (const key in amenities) {
+      newList.push(amenities[key]);
+    }
+    const res = amenities.join(", ");
+    $("div.amenities > h4").text(res);
+    newList = [];
+  });
+});
